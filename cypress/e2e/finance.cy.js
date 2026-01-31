ce15be91725e9d4e658dceff97aaf4bf6b1b8048
Your Name <you@example.com>
2026-01-31 09:31:26 -0300

    Início Cypress

---
+++ cypress/e2e/finance.cy.js
@@ -0,0 +1,35 @@
+
+describe('Transações', () => {
+    it('Cadastrar uma entrada', () => {
+        cy.visit("https://dev-finance.netlify.app/#")
+        cy.contains("+ Nova Transação").click()         //contains(" "): localiza por nomes.    click(): clicar.
+        cy.get('#description').type("Samuel")           //get(" "): pegar.                      type(" "): escrever 
+        cy.get('#amount').type(250)
+        cy.get('#date').type("2025-11-26")              //Data no formato AAAA-MM-DD
+        
+        cy.contains("button", "Salvar").click()         //palavras iguais as do site. maiusculas são diferentes de minusculas 
+        //cy.contains("Salvar").click()
+        
+        //cy.get('.actions > .button').click()
+        //cy.contains("Cancelar").click()
+
+        cy.get("tbody tr td.description").should("have.text", "Samuel") //Seletor a partir do inspecionar(DevTools)
+        //cy.get('.description').should("have.text", "Samuel")          //Seletor a partir do Cypress (Butão Alvo)
+        
+        //cy.get("tbody tr td.income").should("have.text", "R$ 250,00")   //Erro ao localizar o elemento "R$ 250,00"
+        //cy.get('.income').should("have.text", "R$&nbsp;250,00")
+
+    });
+
+    it('Cadastrar uma saída', () => {
+        cy.visit("https://dev-finance.netlify.app/#")
+        cy.contains("+ Nova Transação").click()         
+        cy.get('#description').type("Aluguel")           
+        cy.get('#amount').type(-800)
+        cy.get('#date').type("2025-11-27")
+        cy.contains("button", "Salvar").click()
+         cy.get("tbody tr td.description").should("have.text", "Aluguel")
+    });
+    
+   
+});
